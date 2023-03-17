859e6f83cfac84c51aef5bc24f34a18a76a1f682
Emanuel Maia <tavanomanu@gmail.com>
2023-03-17 13:39:10 +0000

    Update CMake version

---
+++ cmake-build-debug/CMakeFiles/DA_proj1.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/manu/Programs/UNI/DA-feup/DA-proj1/data_structures/Database.cpp" "CMakeFiles/DA_proj1.dir/data_structures/Database.cpp.o" "gcc" "CMakeFiles/DA_proj1.dir/data_structures/Database.cpp.o.d"
+  "/home/manu/Programs/UNI/DA-feup/DA-proj1/data_structures/Graph.cpp" "CMakeFiles/DA_proj1.dir/data_structures/Graph.cpp.o" "gcc" "CMakeFiles/DA_proj1.dir/data_structures/Graph.cpp.o.d"
+  "/home/manu/Programs/UNI/DA-feup/DA-proj1/data_structures/Heap.cpp" "CMakeFiles/DA_proj1.dir/data_structures/Heap.cpp.o" "gcc" "CMakeFiles/DA_proj1.dir/data_structures/Heap.cpp.o.d"
+  "/home/manu/Programs/UNI/DA-feup/DA-proj1/data_structures/Station.cpp" "CMakeFiles/DA_proj1.dir/data_structures/Station.cpp.o" "gcc" "CMakeFiles/DA_proj1.dir/data_structures/Station.cpp.o.d"
+  "/home/manu/Programs/UNI/DA-feup/DA-proj1/data_structures/UFDS.cpp" "CMakeFiles/DA_proj1.dir/data_structures/UFDS.cpp.o" "gcc" "CMakeFiles/DA_proj1.dir/data_structures/UFDS.cpp.o.d"
+  "/home/manu/Programs/UNI/DA-feup/DA-proj1/data_structures/VertexEdge.cpp" "CMakeFiles/DA_proj1.dir/data_structures/VertexEdge.cpp.o" "gcc" "CMakeFiles/DA_proj1.dir/data_structures/VertexEdge.cpp.o.d"
+  "/home/manu/Programs/UNI/DA-feup/DA-proj1/main.cpp" "CMakeFiles/DA_proj1.dir/main.cpp.o" "gcc" "CMakeFiles/DA_proj1.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/DA_proj1.dir/cmake_clean.cmake
@@ -0,0 +1,23 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DA_proj1.dir/data_structures/Database.cpp.o"
+  "CMakeFiles/DA_proj1.dir/data_structures/Database.cpp.o.d"
+  "CMakeFiles/DA_proj1.dir/data_structures/Graph.cpp.o"
+  "CMakeFiles/DA_proj1.dir/data_structures/Graph.cpp.o.d"
+  "CMakeFiles/DA_proj1.dir/data_structures/Heap.cpp.o"
+  "CMakeFiles/DA_proj1.dir/data_structures/Heap.cpp.o.d"
+  "CMakeFiles/DA_proj1.dir/data_structures/Station.cpp.o"
+  "CMakeFiles/DA_proj1.dir/data_structures/Station.cpp.o.d"
+  "CMakeFiles/DA_proj1.dir/data_structures/UFDS.cpp.o"
+  "CMakeFiles/DA_proj1.dir/data_structures/UFDS.cpp.o.d"
+  "CMakeFiles/DA_proj1.dir/data_structures/VertexEdge.cpp.o"
+  "CMakeFiles/DA_proj1.dir/data_structures/VertexEdge.cpp.o.d"
+  "CMakeFiles/DA_proj1.dir/main.cpp.o"
+  "CMakeFiles/DA_proj1.dir/main.cpp.o.d"
+  "DA_proj1"
+  "DA_proj1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DA_proj1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
